d40c14ffb82bcbc1d7bae45f081505327e8d5bf3
Joe Van Dyk <joe@fixieconsulting.com>
2008-11-21 15:16:49 -0800

    Better error handling

--- lib/fixie_shrinker.rb
+++ lib/fixie_shrinker.rb
@@ -9,14 +9,23 @@ module FixieShrinker
   def compress_and_include_javascripts(compressed_file_name, *javascripts)
     compress_to javascript_path(compressed_file_name), *(javascripts.map { |j| javascript_path(j) })
     javascript_include_tag compressed_file_name
+  rescue Exception => e
+    javascript_include_tag(*javascripts) + report_error(e)
   end
   
   def compress_and_include_stylesheets(compressed_file_name, *stylesheets)
     compress_to stylesheet_path(compressed_file_name), *stylesheets.map { |s| stylesheet_path(s) }
     stylesheet_link_tag compressed_file_name
+  rescue Exception => e
+    stylesheet_link_tag(*stylesheets) + report_error(e)
   end
   
   private
+
+  def report_error exception
+    javascript_tag("alert('Fixie Shrinker noticed bad code!  See console.log in browser for details.')") +
+      javascript_tag("console.log('#{escape_javascript(exception.message)}')")
+  end
   
   def compress?
     environments.include?(RAILS_ENV)
